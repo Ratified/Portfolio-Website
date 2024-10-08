8b3a0c5686b0587f39efafd2aa3a76e0cbf3625a
Ratified <kamungegeorge42@gmail.com>
2024-10-08 09:10:33 +0300

    Fixed Marquee

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -1,23 +1,25 @@
 const Marquee = () => {
   return (
-    <marquee className="languages" behavior="scroll" direction="left">
-    HTML&nbsp;&nbsp;&nbsp;&nbsp;
-    CSS3&nbsp;&nbsp;&nbsp;&nbsp;
-    SASS&nbsp;&nbsp;&nbsp;&nbsp;
-    TailwindCSS&nbsp;&nbsp;&nbsp;&nbsp;
-    Javascript&nbsp;&nbsp;&nbsp;&nbsp;
-    Typescript&nbsp;&nbsp;&nbsp;&nbsp;
-    React JS&nbsp;&nbsp;&nbsp;&nbsp;
-    Node JS&nbsp;&nbsp;&nbsp;&nbsp;
-    Express JS&nbsp;&nbsp;&nbsp;&nbsp;
-    PHP&nbsp;&nbsp;&nbsp;&nbsp;
-    Laravel&nbsp;&nbsp;&nbsp;&nbsp;
-    MongoDB&nbsp;&nbsp;&nbsp;&nbsp;
-    MySQL&nbsp;&nbsp;&nbsp;&nbsp;
-    Postgresql&nbsp;&nbsp;&nbsp;&nbsp;
-    Docker&nbsp;&nbsp;&nbsp;&nbsp;
-    CI/CD&nbsp;&nbsp;&nbsp;&nbsp;
-    </marquee>
+    <div className="languages-container">
+      <div className="languages">
+        HTML&nbsp;&nbsp;&nbsp;&nbsp;
+        CSS3&nbsp;&nbsp;&nbsp;&nbsp;
+        SASS&nbsp;&nbsp;&nbsp;&nbsp;
+        TailwindCSS&nbsp;&nbsp;&nbsp;&nbsp;
+        Javascript&nbsp;&nbsp;&nbsp;&nbsp;
+        Typescript&nbsp;&nbsp;&nbsp;&nbsp;
+        React JS&nbsp;&nbsp;&nbsp;&nbsp;
+        Node JS&nbsp;&nbsp;&nbsp;&nbsp;
+        Express JS&nbsp;&nbsp;&nbsp;&nbsp;
+        PHP&nbsp;&nbsp;&nbsp;&nbsp;
+        Laravel&nbsp;&nbsp;&nbsp;&nbsp;
+        MongoDB&nbsp;&nbsp;&nbsp;&nbsp;
+        MySQL&nbsp;&nbsp;&nbsp;&nbsp;
+        Postgresql&nbsp;&nbsp;&nbsp;&nbsp;
+        Docker&nbsp;&nbsp;&nbsp;&nbsp;
+        CI/CD&nbsp;&nbsp;&nbsp;&nbsp;
+      </div>
+    </div>
   )
 }
 
