195f479a25610f630854c611b925aea6af215a71
Ratified <kamungegeorge42@gmail.com>
2024-10-11 07:06:11 +0300

    Responsive Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,57 @@
-import { Link } from "react-router-dom"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import '../css/navbar.css';
 
 const Navbar: React.FC = () => {
+  // Toggle mobile navigation with state
+  const [isMobileNavOpen, setMobileNavOpen] = useState(false);
+
+  const toggleMobileNav = () => {
+    setMobileNavOpen((prev) => !prev);
+  };
+
   return (
     <nav className="navigation">
       <h2>George</h2>
 
       <ul className="navigation__ul">
-        <li><Link to='/'>Home</Link></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#projects">Projects</a></li>
+        <li><Link to='/' className="hideOnMobile">Home</Link></li>
+        <li><a href="#about" className="hideOnMobile">About</a></li>
+        <li><a href="#services" className="hideOnMobile">Services</a></li>
+        <li><a href="#projects" className="hideOnMobile">Projects</a></li>
       </ul>
 
       <div className="navigation__social-media">
-        <a href="#contact-me" className="btn">Hire Me</a>
+        <a href="#contact-me" className="btn" id="hideOnMobile">Hire Me</a>
+        <div className="social-media__icons">
+          <a href="https://github.com/Ratified" className="hideOnMobile"><i className="fab fa-github"></i></a>
+          <a href="https://www.linkedin.com/in/george-kamunge-2a30a5233" className="hideOnMobile"><i className="fab fa-linkedin-in"></i></a>
+        </div>
+      </div>
+
+      {/* Responsive */}
+      <div className="navigation__hamburger" onClick={toggleMobileNav}>
+        <i className="fas fa-bars"></i>
+      </div>
+
+      <div className={`mobile-navigation ${isMobileNavOpen ? 'active' : ''}`}>
+        <div className="mobile-navigation__close" onClick={toggleMobileNav}>
+          <i className="fas fa-times"></i>
+        </div>
+        <ul className="mobile-navigation__ul">
+          <li><Link to='/' onClick={toggleMobileNav}>Home</Link></li>
+          <li><a href="#about" onClick={toggleMobileNav}>About</a></li>
+          <li><a href="#services" onClick={toggleMobileNav}>Services</a></li>
+          <li><a href="#projects" onClick={toggleMobileNav}>Projects</a></li>
+        </ul>
+
         <div className="social-media__icons">
           <a href="https://github.com/Ratified"><i className="fab fa-github"></i></a>
-          <a href="www.linkedin.com/in/george-kamunge-2a30a5233"><i className="fab fa-linkedin-in"></i></a> 
+          <a href="https://www.linkedin.com/in/george-kamunge-2a30a5233"><i className="fab fa-linkedin-in"></i></a>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
