ae90cebfdbbf2d6a634af939c9dac6f6275d0f3c
Ratified <kamungegeorge42@gmail.com>
2024-10-11 19:45:49 +0300

    Final touches

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
     <div className="hero">
-      <h1>Hi, I'm Kamunge</h1>
+      <h1>Hi, I'm <span className="topG">GEORGE KAMUNGE</span></h1>
       <h2>Fullstack Developer | Innovator | Problem Solver</h2>
       <p>
         I specialize in crafting seamless web applications that blend functionality and design. Let’s transform your vision into reality!
